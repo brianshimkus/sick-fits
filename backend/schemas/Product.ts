312a647507c8c14f4b56ad17bd35cb96e5162956
Brian Shimkus <brianshimkus1@gmail.com>
2021-02-12 23:32:50 -0600

    Set up two way data relationships in Keystone

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { integer, select, text } from '@keystone-next/fields';
+import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
@@ -8,8 +8,17 @@ export const Product = list({
     name: text({ isRequired: true }),
     description: text({
       ui: {
-        displayMode: 'textarea'
+        displayMode: 'textarea',
       },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText']},
+      }
     }),
     status: select({
       options: [
