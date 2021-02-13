a8bfbba0b0d0f88b2fa021f368ac45aafb19552f
Brian Shimkus <brianshimkus1@gmail.com>
2021-02-12 23:02:28 -0600

    Created Products data type

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,8 +1,9 @@
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
@@ -36,6 +37,7 @@ export default withAuth(config({
   },
   lists: createSchema({
     User,
+    Product,
   }),
   ui: {
     // Show the UI only for people who pass this test
