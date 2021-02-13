68d598bdea9fdf307de57763d559a20d128f6d74
Brian Shimkus <brianshimkus1@gmail.com>
2021-02-12 22:50:45 -0600

    User authentication set up

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,8 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import 'dotenv/config';
 import { User } from './schemas/User';
+import 'dotenv/config';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
@@ -10,7 +12,17 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 }
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // Todo: Add in initial roles here
+  }
+})
+
+export default withAuth(config({
   server: {
     cors: {
       origin: [process.env.FRONTEND_URL],
@@ -23,11 +35,17 @@ export default config({
     // Todo: Add data seeding here
   },
   lists: createSchema({
-    User
+    User,
   }),
   ui: {
-    // Todo: Change this for roles
-    isAccessAllowed: () => true,
+    // Show the UI only for people who pass this test
+    isAccessAllowed: ({ session }) => {
+      return !!session?.data;
+    },
   },
-  // Todo: Add session values here
-});
+  session: withItemData(statelessSessions(sessionConfig), {
+    // GraphQL Query
+    User: 'id',
+  }),
+})
+);
