145ae24d57bcaffe999ad20c64146ee41cf17c11
Brian Shimkus <brianshimkus1@gmail.com>
2021-02-12 22:21:20 -0600

    Set up Keystone and Typescript

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long to let the user stay signed in
+  secret: process.env.COOKIE_SECRET,
+}
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // Todo: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    // Todo: Change this for roles
+    isAccessAllowed: () => true,
+  },
+  // Todo: Add session values here
+});
