8362e73203403f239be224e2644e8300e90cfe29
Brian Shimkus <brianshimkus1@gmail.com>
2021-02-13 23:07:16 -0600

    Added a styled component to Products component

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_PRODUCTS_QUERY = gql`
 	query ALL_PRODUCTS_QUERY {
@@ -18,6 +19,12 @@ const ALL_PRODUCTS_QUERY = gql`
 	}
 `;
 
+const ProductsList = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-gap: 60px;
+`;
+
 export default function Products() {
 	const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
 	console.log(data, error, loading);
@@ -25,9 +32,11 @@ export default function Products() {
 	if (error) return <p>Error: {error.message}</p>;
 	return (
 		<div>
-			{data.allProducts.map((product) => (
-				<p>{product.name}</p>
-			))}
+			<ProductsList>
+				{data.allProducts.map((product) => (
+					<p key={product.id}>{product.name}</p>
+				))}
+			</ProductsList>
 		</div>
 	);
 }
