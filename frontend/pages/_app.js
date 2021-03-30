dbd6d28ed55584f95dccdfce87bf95e35e21e0ce
Brian Shimkus <brianshimkus1@gmail.com>
2021-03-29 22:40:43 -0500

    Installed apollo and fixed error

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,30 @@
-import NProgress from 'nprogress';
-import Page from '../components/Page';
-import Router from 'next/router';
+import NProgress from 'nprogress'
+import Page from '../components/Page'
+import Router from 'next/router'
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client'
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
-import '../components/styles/nprogress.css';
+import '../components/styles/nprogress.css'
+
+const client = new ApolloClient({
+	link: createHttpLink({ uri: 'http://localhost:3000' }),
+	cache: new InMemoryCache(),
+})
 
 export default function MyApp({ Component, pageProps }) {
 	return (
-		<Page>
-			<Component {...pageProps} />
-		</Page>
-	);
+		<ApolloProvider client={client}>
+			<Page>
+				<Component {...pageProps} />
+			</Page>
+		</ApolloProvider>
+	)
 }
