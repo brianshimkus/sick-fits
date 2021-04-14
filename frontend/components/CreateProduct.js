b96758ca86d5ff703f02a90856b66dddceb2b20d
Brian Shimkus <brianshimkus1@gmail.com>
2021-04-13 22:57:21 -0500

    Implemented useForm functions into CreateProduct

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import useForm from '../lib/useForm'
 
 export default function CreateProduct() {
-	const [name, setName] = useState('Brian')
+	const { inputs, handleChange, clearForm, resetForm } = useForm({
+		name: 'This is a name',
+		price: 34,
+		description: 'This is a description',
+	})
 	return (
 		<form>
 			<label htmlFor='name'>
@@ -11,12 +15,39 @@ export default function CreateProduct() {
 					id='name'
 					name='name'
 					placeholder='Name'
-					value={name}
-					onChange={(e) => {
-						setName(e.target.value)
-					}}
+					value={inputs.name}
+					onChange={handleChange}
 				/>
 			</label>
+			<label htmlFor='price'>
+				Price
+				<input
+					type='number'
+					id='price'
+					name='price'
+					placeholder='Price'
+					value={inputs.price}
+					onChange={handleChange}
+				/>
+			</label>
+			<label htmlFor='description'>
+				Description
+				<input
+					type='text'
+					id='description'
+					name='description'
+					placeholder='Description'
+					value={inputs.description}
+					onChange={handleChange}
+				/>
+			</label>
+
+			<button type='button' onClick={clearForm}>
+				Clear Form
+			</button>
+			<button type='button' onClick={resetForm}>
+				Reset Form
+			</button>
 		</form>
 	)
 }
