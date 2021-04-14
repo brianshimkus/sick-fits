f88bfa34a629671f26e64ef6d964dd1b405cdf4c
Brian Shimkus <brianshimkus1@gmail.com>
2021-04-13 22:35:49 -0500

    Created CreateProduct component

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export default function CreateProduct() {
+	const [name, setName] = useState('Brian')
+	return (
+		<form>
+			<label htmlFor='name'>
+				Name
+				<input
+					type='text'
+					id='name'
+					name='name'
+					placeholder='Name'
+					value={name}
+					onChange={(e) => {
+						setName(e.target.value)
+					}}
+				/>
+			</label>
+		</form>
+	)
+}
