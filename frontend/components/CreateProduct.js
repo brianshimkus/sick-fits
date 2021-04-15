97716cd8333bd8f5b8ffb4c3da51600b90f8a67a
Brian Shimkus <brianshimkus1@gmail.com>
2021-04-14 23:08:55 -0500

    Hooked up File input and Form styles

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,53 +1,66 @@
 import useForm from '../lib/useForm'
+import Form from './styles/Form'
 
 export default function CreateProduct() {
 	const { inputs, handleChange, clearForm, resetForm } = useForm({
+		image: '',
 		name: 'This is a name',
 		price: 34,
 		description: 'This is a description',
 	})
 	return (
-		<form>
-			<label htmlFor='name'>
-				Name
-				<input
-					type='text'
-					id='name'
-					name='name'
-					placeholder='Name'
-					value={inputs.name}
-					onChange={handleChange}
-				/>
-			</label>
-			<label htmlFor='price'>
-				Price
-				<input
-					type='number'
-					id='price'
-					name='price'
-					placeholder='Price'
-					value={inputs.price}
-					onChange={handleChange}
-				/>
-			</label>
-			<label htmlFor='description'>
-				Description
-				<input
-					type='text'
-					id='description'
-					name='description'
-					placeholder='Description'
-					value={inputs.description}
-					onChange={handleChange}
-				/>
-			</label>
+		<Form
+			onSubmit={(e) => {
+				e.preventDefault()
+				console.log(inputs)
+			}}
+		>
+			<fieldset>
+				<label htmlFor='name'>
+					Image
+					<input
+						required
+						type='file'
+						id='image'
+						name='image'
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor='name'>
+					Name
+					<input
+						type='text'
+						id='name'
+						name='name'
+						placeholder='Name'
+						value={inputs.name}
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor='price'>
+					Price
+					<input
+						type='number'
+						id='price'
+						name='price'
+						placeholder='Price'
+						value={inputs.price}
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor='description'>
+					Description
+					<textarea
+						id='description'
+						name='description'
+						placeholder='Description'
+						value={inputs.description}
+						onChange={handleChange}
+					/>
+				</label>
 
-			<button type='button' onClick={clearForm}>
-				Clear Form
-			</button>
-			<button type='button' onClick={resetForm}>
-				Reset Form
-			</button>
-		</form>
+				<button type='submit'>+ Add Product</button>
+			</fieldset>
+		</Form>
 	)
 }
