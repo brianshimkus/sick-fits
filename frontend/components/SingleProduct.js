a86ba2b4620335aa7e7e6f47f101ab698d3dfd9d
Brian Shimkus <brianshimkus1@gmail.com>
2021-05-03 22:49:48 -0500

    Created SingleProduct component

---
+++ frontend/components/SingleProduct.js
@@ -0,0 +1,23 @@
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+import DisplayError from './ErrorMessage'
+
+const SINGLE_ITEM_QUERY = gql`
+	query {
+		Product(where: { id: "" }) {
+			name
+			price
+			description
+		}
+	}
+`
+export default function SingleProduct({ id }) {
+	const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY)
+	if (loading) return <p>Loading...</p>
+	if (error) return <DisplayError error={error} />
+	return (
+		<div>
+			<h2>{data.Product.name}</h2>
+		</div>
+	)
+}
