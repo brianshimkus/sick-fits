3b6b0c848253549d214321e3d955d71f414ce0dc
Brian Shimkus <brianshimkus1@gmail.com>
2021-05-04 18:03:10 -0500

    Got the mutation to update product

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,9 +1,28 @@
-import { useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
 
 const SINGLE_PRODUCT_QUERY = gql`
 	query SINGLE_PRODUCT_QUERY($id: ID!) {
 		Product(where: { id: $id }) {
+			id
+			name
+			description
+			price
+		}
+	}
+`
+
+const UPDATE_PRODUCT_MUTATION = gql`
+	mutation UPDATE_PRODUCT_MUTATION(
+		$id: ID!
+		$name: String
+		$description: String
+		$price: Int
+	) {
+		updateProduct(
+			id: $id
+			data: { id: $id, name: $name, description: $description, price: $price }
+		) {
 			id
 			name
 			description
@@ -19,6 +38,15 @@ export default function UpdateProduct({ id }) {
 	})
 	console.log(data)
 	// 2. Need to get the mutation to update product
+	const [
+		updateProduct,
+		{ data: updateData, error: updateError, loading: updateLoading },
+	] = useMutation(UPDATE_PRODUCT_MUTATION, {
+		variables: {
+			id: id,
+			// Pass in updates to product here
+		},
+	})
 	// 3. Need the form to handle the updates
 	return <p>Update {id}!</p>
 }
